b0b6ce5ca5ceeef95ca8636b89e28f24b314bcfd
Mt.Du <justin.d128@gmail.com>
2016-11-17 17:23:07 -0600

    Added all files

---
+++ JS/global.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {	
+	$( ".back-to-top" ).click(function() {
+	    scrollTo(0,0);
+	});
+	$('#registrationLink').on('click', function(e) { e.preventDefault(); });
+});
+
+function generateSchoolCalendar() {
+    var tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
+    tab_text = tab_text + '<head><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>';
+
+    tab_text = tab_text + '<x:Name>School Calendar</x:Name>';
+
+    tab_text = tab_text + '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>';
+    tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+
+    tab_text = tab_text + "<table border='1px'>";
+    tab_text = tab_text + $('#schoolCalendar').html();
+    tab_text = tab_text + '</table></body></html>';
+
+    var data_type = 'data:application/vnd.ms-excel';
+    
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        if (window.navigator.msSaveBlob) {
+            var blob = new Blob([tab_text], {
+                type: "application/csv;charset=utf-8;"
+            });
+            navigator.msSaveBlob(blob, 'SchoolCalendar.xls');
+        }
+    } else {
+        $('#excelSchoolCalendar').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
+        $('#excelSchoolCalendar').attr('download', 'SchoolCalendar.xls');
+    }
+
+}
+
+function generateBoardCalendar() {
+    var tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
+    tab_text = tab_text + '<head><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>';
+
+    tab_text = tab_text + '<x:Name>Board Members Calendar</x:Name>';
+
+    tab_text = tab_text + '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>';
+    tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+
+    tab_text = tab_text + "<table border='1px'>";
+    tab_text = tab_text + $('#boardCalendar').html();
+    tab_text = tab_text + '</table></body></html>';
+
+    var data_type = 'data:application/vnd.ms-excel';
+    
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        if (window.navigator.msSaveBlob) {
+            var blob = new Blob([tab_text], {
+                type: "application/csv;charset=utf-8;"
+            });
+            navigator.msSaveBlob(blob, 'BoardMembersCalendar.xls');
+        }
+    } else {
+        $('#excelBoardCalendar').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
+        $('#excelBoardCalendar').attr('download', 'BoardMembersCalendar.xls');
+    }
+
+}
